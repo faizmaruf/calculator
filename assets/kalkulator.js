a99481098cfebd41671d1dcd42d7d3e8fda53ccb
Faiz Ma'ruf <faiz.maruf8888@gmail.com>
2020-08-31 13:06:08 +0700

    update

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -93,3 +93,28 @@ for (let button of buttons) {
         updateDisplay()
     });
 }
+
+function performCalculation() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+        alert("Anda belum menetapkan operator");
+        return;
+    }
+
+    let result = 0;
+    if (calculator.operator === "+") {
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else {
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber)
+    }
+
+    // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+    putHistory(history);
+    calculator.displayNumber = result;
+    renderHistory();
+}
